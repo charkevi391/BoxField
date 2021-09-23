50eb248e9077c584f465229ff535e3c6c6f7488d
Charlie K <charlie.kevill@outlook.com>
2021-09-23 11:09:41 -0400

    2r4t5y

---
+++ BoxField/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxField
+{
+    class Box
+    {
+        public int x, y, size, speed, color;
+
+        public Box(int _x, int _y, int _size, int _speed, int _color)
+        {
+            x = _x;
+            y = _y;
+            size = _size;   
+            speed = _speed;
+            color = _color; 
+        }
+    }
+}
--- BoxField/Screens/GameScreen.cs
+++ BoxField/Screens/GameScreen.cs
@@ -16,14 +16,18 @@ namespace BoxField
         Boolean leftArrowDown, rightArrowDown;
 
         //used to draw boxes on screen
-        SolidBrush boxBrush = new SolidBrush(Color.White);
+        SolidBrush[] ColorArray = new SolidBrush[8];
 
         //create a list to hold a column of boxes   
+        List<Box> boxesLeft = new List<Box>();
 
+        int newboxCounter = 0;
 
+        Random randGen = new Random();
         public GameScreen()
         {
             InitializeComponent();
+            OnStart();
         }
 
         /// <summary>
@@ -31,7 +35,22 @@ namespace BoxField
         /// </summary>
         public void OnStart()
         {
-            //TODO - set game start values
+            ColorArray[0] = new SolidBrush(Color.White);
+            ColorArray[1] = new SolidBrush(Color.Blue);
+            ColorArray[2] = new SolidBrush(Color.Green);
+            ColorArray[3] = new SolidBrush(Color.Purple);
+            ColorArray[4] = new SolidBrush(Color.Red);
+            ColorArray[5] = new SolidBrush(Color.Orange);
+            ColorArray[6] = new SolidBrush(Color.Pink);
+            ColorArray[7] = new SolidBrush(Color.Black);
+            int xval = randGen.Next(10, 100);
+            int cval = randGen.Next(0, 8);
+            //TODO - set game start value
+            Box b = new Box(xval, 0, 20, 10, cval);
+            Box b2 = new Box(xval + 200, 0, 20, 10, cval);
+            boxesLeft.Add(b);
+            boxesLeft.Add(b2);
+
         }
 
         private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
@@ -44,25 +63,61 @@ namespace BoxField
                     break;
                 case Keys.Right:
                     rightArrowDown = true;
-                    break;           
+                    break;
             }
         }
 
 
         private void gameLoop_Tick(object sender, EventArgs e)
         {
+            int xval = randGen.Next(10, 100);
+            int cval = randGen.Next(0, 8);
+            newboxCounter++;
             //TODO - update location of all boxes (drop down screen)
+            foreach (Box b in boxesLeft)
+            {
+                b.y += b.speed;
+            }
 
-            //TODO - remove box if it has gone of screen
+            foreach (Box b2 in boxesLeft)
+            {
+                b2.y += b2.speed;
+            }
 
-            //TODO - add new box if it is time
+            if (boxesLeft[0].y > 500)
+            {
+                boxesLeft.RemoveAt(0); //-remove box if it has gone of screen
+            }
 
+            Box box = new Box(xval, 0, 20, 10, cval);
+            if (newboxCounter == 2) //-add new box if it is time
+            { 
+                boxesLeft.Add(box);
+                boxesLeft.Add(box2);
+                newboxCounter = 0;
+            }
+
+            Box box2 = new Box(xval + 200, 0, 20, 10, cval);
+            if (newboxCounter == 2) //-add new box if it is time
+            {
+
+                newboxCounter = 0;
+            }
             Refresh();
         }
 
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
             //TODO - draw boxes to screen
+            foreach (Box b in boxesLeft)
+            {
+                e.Graphics.FillRectangle(ColorArray[b.color], b.x, b.y, b.size, b.size);
+            }
+
+            foreach (Box b2 in boxesLeft)
+            {
+                e.Graphics.FillRectangle(ColorArray[b2.color], b2.x, b2.y, b2.size, b2.size);
+            }
         }
     }
 }
